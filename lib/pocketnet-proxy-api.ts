98a4fd2c79c29b9adba222b830d9f4adac6cc68c
daniil <dan.kim.04lb@gmail.com>
2024-12-17 16:43:17 +0500

    fix: remove test code

--- lib/pocketnet-proxy-api.ts
+++ lib/pocketnet-proxy-api.ts
@@ -198,26 +198,4 @@ class PocketNetProxyApi {
   }
 }
 
-async function a() {
-  const p = await PocketNetProxyApi.create()
-  setTimeout(async () => {
-    const d = await p.wallet.getBalanceWithPrivateKey({
-      key: '5vNa1YiHTGHe3xhX3xW9kdAbUTh8AmTtzn5b74meC1uwQPVw9BtD',
-    })
-
-    // txid - id transaction
-    const f = await p.wallet.sendFundsWithPrivateKey({
-      address: 'PC1kpAnbE4ovPXWGEzDtPW1fGP6XsqcVgZ',
-      amount: 2,
-      key: '5vNa1YiHTGHe3xhX3xW9kdAbUTh8AmTtzn5b74meC1uwQPVw9BtD',
-      feemode: 'include',
-    })
-    console.log(d)
-  }, 3000)
-
-  // console.log(d, '||')
-}
-
-a()
-
 export default PocketNetProxyApi
